a5338830259b29a9d5d32d6e427fc34ec6806fbc
Pepita Kautto <pepita.kautto@gmail.com>
2018-10-30 11:21:30 +0200

    pistekautta poistettu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Component} from 'react';
 import ReactDOM from 'react-dom';
-import "./app.css";
+import "app.css";
 
 // pisteiden lasku ulkona funktiosta, ettei nollaannu joka kierroksella
 var pisteet = 0;
